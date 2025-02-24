6c7e7859b670124b41258ea5ca58c4710b737b67
Giovani Silva <giovani-silva1@live.com>
2025-02-23 22:08:35 -0300

    Criado um componente filho para receber dados das coordenadas

--- src/app/modules/weather/componentes/weather-card/weather-card.component.ts
+++ src/app/modules/weather/componentes/weather-card/weather-card.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { WeatherDatas } from 'src/app/models/interfaces/WeatherDatas';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-weather-card',
   templateUrl: './weather-card.component.html',
   styleUrls: [],
 })
-export class WeatherCardComponent {}
+export class WeatherCardComponent implements OnInit {
+  @Input() weatherDatasInput!: WeatherDatas;
+  ngOnInit(): void {
+    console.log(
+      'DADOS RECEBIDOS DO COMPONENTE PAI - WHEATHER CARD:',
+      this.weatherDatasInput
+    );
+  }
+}
